0dc32f35757bf8d4d69b9f161c05dca926c055ab
Lee Sang-Min <sangmin4208@gmail.com>
2022-01-09 23:59:07 +0900

    mend

--- new.sh
+++ new.sh
@@ -1,10 +1,7 @@
 echo "Make new branch name: $1 ✨"
 git checkout --orphan $1
-echo "cleaning...🧹"
+echo "Cleaning...🧹"
 git rm --cached -r .
 rm -rf ./*
-if [ $2 = "--create" ]
 echo "Create New Vue Project 👑"
-then
-  vue create .
-fi
+vue create .
