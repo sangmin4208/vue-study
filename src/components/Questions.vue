7bae98cfb5fe23d75b4dc694d1d06baf834a2338
Lee Sang-Min <sangmin4208@gmail.com>
2022-01-12 15:27:05 +0900

    Done

---
+++ src/components/Questions.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionsAnswered * 100) / questions.length}%` }"
+      ></div>
+      <div class="status">
+        {{ questionsAnswered }} out of {{ questions.length }} questions answered
+      </div>
+    </div>
+    <div
+      class="single-question"
+      v-for="(question, qi) in questions"
+      :key="question.q"
+      v-show="questionsAnswered === qi"
+    >
+      <div class="question">
+        {{ question.q }}
+      </div>
+      <div class="answers">
+        <div
+          class="answer"
+          v-for="answer in question.answers"
+          :key="answer.text"
+          @click.prevent="selectAnswer(answer.is_correct)"
+        >
+          {{ answer.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['questions', 'questionsAnswered'],
+  emits: ['question-answered'],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit('question-answered', is_correct)
+    },
+  },
+}
+</script>
+
+<style></style>
